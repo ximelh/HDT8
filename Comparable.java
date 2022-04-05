bdb2131c10feeb7c7c402a0474e9efc9a5c632c8
ximelh <ximenaloarca@gmail.com>
2022-04-05 08:24:40 -0600

    Interfaces

---
+++ Comparable.java
@@ -0,0 +1,8 @@
+/**
+ * Comparable.java
+ * @since 4/5/22
+ */
+public interface Comparable<E>
+{
+public int compareTo(E that);
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main.java
+ * @since 4/5/22
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
