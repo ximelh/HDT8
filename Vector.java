b0f9c34d0c830fe4502f54b8c566fdece22c7981
ximelh <ximenaloarca@gmail.com>
2022-04-05 09:23:50 -0600

    Vector Heap

---
+++ Vector.java
@@ -0,0 +1,83 @@
+public class Vector<E extends Comparable<E>> implements PriorityQueue<E>{
+
+    @Override
+    public E getFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void add(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+
+    protected Vector<E> data;
+
+    public VectorHeap(){
+        data= new Vector<E>();
+    }
+
+
+    public VectorHeap(Vector<E> v)
+    // post: constructs a new priority queue from an unordered vector
+    {
+        int i;
+        data = new Vector<E>(v.size()); // we know ultimate size
+        for (i = 0; i < v.size(); i++)
+        { // add elements to heap
+            add(v.get(i));
+        }
+    }
+
+
+    protected static int parent(int i)
+    // pre: 0 <= i < size
+    // post: returns parent of node at location i
+    {
+        return (i-1)/2;
+    }
+
+
+    protected static int left(int i)
+    // pre: 0 <= i < size
+    // post: returns index of left child of node at location i
+    {
+        return 2*i+1;
+    }
+    
+    
+    protected static int right(int i)
+    // pre: 0 <= i < size
+    // post: returns index of right child of node at location i
+    {
+        return 2*(i+1);
+    }
+
+}
